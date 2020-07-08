afedef23d5d54af76f867245a71ebf72d6a5e8d8
Zablas <vagabaliukas@gmail.com>
2020-07-08 13:24:37 +0300

    Added a Contact class

---
+++ Visma_Intro/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace Visma_Intro
+{
+    /// <summary>
+    /// The class to store information about a single contact
+    /// </summary>
+    class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+}
